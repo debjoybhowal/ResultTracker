8b0fd38874861ec02770b46e72beb29e8956ef9e
Pritam Chatterjee <pritamchatterjee2009@gmail.com>
2020-10-22 09:44:53 +0530

    new Changes by Pritam

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'dash',
+    loadChildren: () => import('./dash/dash.module').then((m) => m.DashModule),
+  },
+  {
+    path: '',
+    redirectTo: '/dash/home',
+    pathMatch:'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
