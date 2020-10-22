8e03cfbda44f6d76f2ecf56db02091839ac67c28
Pritam Chatterjee <pritamchatterjee2009@gmail.com>
2020-10-22 12:53:01 +0530

    Validator added 22/10/2020

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormArray,FormBuilder,FormGroup,FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  form;
+  constructor(private fb: FormBuilder) { 
+    this.form = this.fb.group({
+      'username':new FormControl('',[
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(13)
+      ]),
+      'studentname':new FormControl('',[
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30)
+      ]),
+      'primaryEmail': new FormControl('',[
+        Validators.required,
+        Validators.email
+      ])
+    })
+  }
+  get user_name(){
+    return this.form.get('username');
+  }
+  get student_name(){
+    return this.form.get('studentname');
+  }
+  get primary_Email() {
+    return this.form.get('primaryEmail');
+} 
   ngOnInit(): void {
   }
 
