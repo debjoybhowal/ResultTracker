c6b45866d913d0db4bb9caf8e1f32e0fb0e6b8e7
Debjoy <debjoybhowal7@gmail.com>
2020-11-03 10:06:30 +0530

    Shashank added edit and delete of subjects

--- src/app/dash/subject/subject.component.ts
+++ src/app/dash/subject/subject.component.ts
@@ -1,8 +1,8 @@
-import { Location } from '@angular/common';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import {
+import {​ Location }​ from '@angular/common';
+import {​ Component, OnInit, ViewChild }​ from '@angular/core';
+import {​ FormControl, FormGroup, Validators }​ from '@angular/forms';
+import {​ ActivatedRoute }​ from '@angular/router';
+import {​
   ApexAxisChartSeries,
   ApexChart,
   ChartComponent,
@@ -12,10 +12,9 @@ import {
   ApexTitleSubtitle,
   ApexXAxis,
   ApexFill,
-} from 'ng-apexcharts';
-import { SubjectService } from './subject.service';
-
-export type ChartOptions = {
+}​ from 'ng-apexcharts';
+import {​ SubjectService }​ from './subject.service';
+export type ChartOptions = {​
   series: ApexAxisChartSeries;
   chart: ApexChart;
   dataLabels: ApexDataLabels;
@@ -24,13 +23,13 @@ export type ChartOptions = {
   xaxis: ApexXAxis;
   fill: ApexFill;
   title: ApexTitleSubtitle;
-};
-@Component({
+}​;
+@Component({​
   selector: 'app-subject',
   templateUrl: './subject.component.html',
   styleUrls: ['./subject.component.css'],
-})
-export class SubjectComponent implements OnInit {
+}​)
+export class SubjectComponent implements OnInit {​
   subjectListWithMarks;
   user_id: string;
   pwd: string;
@@ -38,9 +37,12 @@ export class SubjectComponent implements OnInit {
   subjectListAny;
   subjectLoaded: string = '';
   termList;
-
+  current_id;
+  current_sub;
+  current_term;
   showSubjectAddModal: boolean = false;
-
+  deleteSubjectAddModal: boolean = false;
+  showSubjectEditModal :boolean=false;
   paramLoaded: boolean = false;
   @ViewChild('chart1') chart1: ChartComponent;
   public chartOptions1: Partial<ChartOptions>;
@@ -50,119 +52,116 @@ export class SubjectComponent implements OnInit {
     private subjectService: SubjectService,
     private route: ActivatedRoute,
     private location: Location
-  ) {}
-
-  addSubjectForm: FormGroup = new FormGroup({
+  ) {​}​
+  addSubjectForm: FormGroup = new FormGroup({​
     sub_name: new FormControl('', Validators.required),
     term_id: new FormControl('',Validators.required),
-  });
-  ngOnInit() {
-    if (localStorage.getItem('user_id')) {
+  }​);
+  editSubjectForm: FormGroup = new FormGroup({​
+    sub_name: new FormControl('', Validators.required),
+  }​);
+  ngOnInit() {​
+    if (localStorage.getItem('user_id')) {​
       this.user_id = localStorage.getItem('user_id');
       this.pwd = localStorage.getItem('pwd');
-
-      this.route.paramMap.subscribe((route: any) => {
-        if (route.params.id) {
+      this.route.paramMap.subscribe((route: any) => {​
+        if (route.params.id) {​
           this.loadSpecificSubject(route.params.id);
-        }
-      });
-
+        }​
+      }​);
       this.subjectService
         .getAllSubjectData(this.user_id, this.pwd)
-        .subscribe((response: any) => {
-          if (!this.paramLoaded && response.response.length > 0){            
+        .subscribe((response: any) => {​
+          if (!this.paramLoaded && response.response && response.response.length > 0){​            
             this.location.go('/dash/subject/' + response.response[0].sub_id);
             this.loadSpecificSubject( response.response[0].sub_id);
-          }
+          }​
           this.subjectList = response;
           this.chartData2();
-        });
+        }​);
       this.loadSubjectListAny();
-    }
-  }
-  loadSpecificSubject(id){
+    }​
+  }​
+  loadSpecificSubject(id){​
       this.paramLoaded = true;
       this.chartOptions1 = undefined;
-
       this.subjectListWithMarks = undefined;
       this.subjectService
         .getSubjectFromId(this.user_id, this.pwd,id)
-        .subscribe((data: any) => {
+        .subscribe((data: any) => {​
           this.subjectListWithMarks = data;
           this.chartData1();
-        });
-  }
-  loadSubjectListAny() {
+        }​);
+  }​
+  loadSubjectListAny() {​
     this.subjectListAny = undefined;
     this.subjectService
       .getSubjectList(this.user_id, this.pwd)
-      .subscribe((response: any) => {
+      .subscribe((response: any) => {​
         this.subjectListAny = response.response;
-        if (this.subjectListAny.length <= 0) {
-          this.chartOptions1 = {};
-          this.subjectListWithMarks = {response:[]};
-        }
-      });
-  }
-  chartData1() {
+        if (this.subjectListAny && this.subjectListAny.length <= 0) {​
+          this.chartOptions1 = {​}​;
+          this.subjectListWithMarks = {​response:[]}​;
+        }​
+      }​);
+  }​
+  chartData1() {​
     let posts1;
     posts1 = this.subjectListWithMarks.response;
     let termsChart = [];
     let marks1 = [];
     this.subjectLoaded = posts1.length > 0 ? posts1[0].subject_name : '';
-    posts1.forEach((character) => {
+    posts1.forEach((character) => {​
       termsChart.push(
         character.assesment_number > 1
           ? character.exam_name + ' ' + character.assesment_number
           : character.exam_name
       );
       marks1.push(character.percentage);
-    });
-
-    this.chartOptions1 = {
+    }​);
+    this.chartOptions1 = {​
       series: [
-        {
+        {​
           name: 'Score',
           data: marks1,
-        },
+        }​,
       ],
-      chart: {
+      chart: {​
         height: 350,
         type: 'bar',
-      },
-      plotOptions: {
-        bar: {
+      }​,
+      plotOptions: {​
+        bar: {​
           columnWidth: '50%',
           endingShape: 'flat',
-        },
-      },
-      dataLabels: {
+        }​,
+      }​,
+      dataLabels: {​
         enabled: true,
-        formatter: function (val) {
+        formatter: function (val) {​
           return val + '%';
-        },
+        }​,
         offsetY: 20,
-        style: {
+        style: {​
           fontSize: '12px',
           colors: ['#ffffff'],
-        },
-      },
-
-      xaxis: {
-        labels: {
+        }​,
+      }​,
+      xaxis: {​
+        labels: {​
           rotate: -45,
-        },
+        }​,
         categories: termsChart,
         tickPlacement: 'on',
-      },
-      yaxis: {
-        title: {
+      }​,
+      yaxis: {​
+        title: {​
           text: 'Score',
-        },
-      },
-      fill: {
+        }​,
+      }​,
+      fill: {​
         type: 'gradient',
-        gradient: {
+        gradient: {​
           shade: 'light',
           type: 'horizontal',
           shadeIntensity: 0.25,
@@ -171,66 +170,64 @@ export class SubjectComponent implements OnInit {
           opacityFrom: 0.85,
           opacityTo: 0.85,
           stops: [50, 0, 100],
-        },
-      },
-    };
-  }
-  chartData2() {
+        }​,
+      }​,
+    }​;
+  }​
+  chartData2() {​
     let overall;
     let posts;
     let percent = [];
     let termsChart = [];
     overall = this.subjectList.average.response;
     posts = this.subjectList.response;
-    posts.forEach((character) => {
+    posts.forEach((character) => {​
       termsChart.push(character.subject_name);
       percent.push(Math.round(character.average * 10) / 10);
-    });
-
-    this.chartOptions2 = {
+    }​);
+    this.chartOptions2 = {​
       series: [
-        {
+        {​
           name: 'Score',
           data: percent,
-        },
+        }​,
       ],
-      chart: {
+      chart: {​
         height: 350,
         type: 'bar',
-      },
-      plotOptions: {
-        bar: {
+      }​,
+      plotOptions: {​
+        bar: {​
           columnWidth: '50%',
           endingShape: 'flat',
-        },
-      },
-      dataLabels: {
+        }​,
+      }​,
+      dataLabels: {​
         enabled: true,
-        formatter: function (val) {
+        formatter: function (val) {​
           return val + '%';
-        },
+        }​,
         offsetY: 20,
-        style: {
+        style: {​
           fontSize: '12px',
           colors: ['#ffffff'],
-        },
-      },
-
-      xaxis: {
-        labels: {
+        }​,
+      }​,
+      xaxis: {​
+        labels: {​
           rotate: -45,
-        },
+        }​,
         categories: termsChart,
         tickPlacement: 'on',
-      },
-      yaxis: {
-        title: {
+      }​,
+      yaxis: {​
+        title: {​
           text: 'Score',
-        },
-      },
-      fill: {
+        }​,
+      }​,
+      fill: {​
         type: 'gradient',
-        gradient: {
+        gradient: {​
           shade: 'light',
           type: 'horizontal',
           shadeIntensity: 0.25,
@@ -239,45 +236,83 @@ export class SubjectComponent implements OnInit {
           opacityFrom: 0.85,
           opacityTo: 0.85,
           stops: [50, 0, 100],
-        },
-      },
-      title: {
+        }​,
+      }​,
+      title: {​
         text: 'student marks' + 'having overall average of ' + ' ' + overall,
-
         offsetY: 320,
         align: 'center',
-        style: {
+        style: {​
           color: '#444',
-        },
-      },
-    };
-  }
-
-  onAdd() {
+        }​,
+      }​,
+    }​;
+  }​
+  onAdd() {​
     this.showSubjectAddModal = false;
     this.subjectService
       .addsub(this.user_id, this.pwd, this.addSubjectForm.value)
-      .subscribe((response: any) => {
+      .subscribe((response: any) => {​
         this.loadSubjectListAny();
-      });
-  }
-
-  changeTermId(e) {
+      }​);
+  }​
+  changeTermId(e) {​
     console.log(e.target.value);
-    this.addSubjectForm.get('term_id').setValue(e.target.value, {
+    this.addSubjectForm.get('term_id').setValue(e.target.value, {​
       onlySelf: true,
-    });
-  }
-
-  openSubjectModal() {
+    }​);
+  }​
+  editTermId(e) {​
+    console.log(e.target.value);
+    this.editSubjectForm.get('term_id').setValue(e.target.value, {​
+      onlySelf: true,
+    }​);
+  }​
+  openSubjectModal() {​
     this.addSubjectForm.markAsUntouched();
     this.addSubjectForm.get("sub_name").reset();  
     this.termList = undefined;
     this.showSubjectAddModal = true;
     this.subjectService
       .getTermList(this.user_id, this.pwd)
-      .subscribe((response: any) => {
+      .subscribe((response: any) => {​
         this.termList = response.response;
-      });
-  }
-}
+      }​);
+  }​
+  opendelSubjectModal(id,name){​
+    this.deleteSubjectAddModal=true;
+this.current_id=id;
+this.current_sub=name;
+  }​
+  openEditSubjectModal(id,name,term){​
+    this. showSubjectEditModal=true;
+    this.current_id=id;
+    this.current_sub=name;
+    this.current_term=term;
+    this.editSubjectForm.get("sub_name").setValue(this.current_sub);
+    this.subjectService
+      .getTermList(this.user_id, this.pwd)
+      .subscribe((response: any) => {​
+        this.termList = response.response;
+      }​);
+}​
+  ondel() {​
+    this.deleteSubjectAddModal = false;
+    this.subjectService.delsub(this.user_id,this.pwd,this.current_id).subscribe((response:any)=>{​
+this.loadSubjectListAny();
+console.log(response);
+    }​);
+  }​
+  onEdit(){​
+    this.showSubjectEditModal=false;
+    this.subjectService.editsub(this.user_id,this.pwd,this.current_id,this.editSubjectForm.value.sub_name).subscribe((response:any)=>{​
+      this.loadSubjectListAny();
+      console.log(response);
+          }​);
+    this.loadSubjectListAny();}​
+}​
+    
+    
+  
+  
+
--- src/app/dash/subject/subject.service.ts
+++ src/app/dash/subject/subject.service.ts
@@ -1,49 +1,55 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
-@Injectable({
+import {​ HttpClient }​ from '@angular/common/http';
+import {​ Injectable }​ from '@angular/core';
+@Injectable({​
   providedIn: 'root',
-})
-export class SubjectService {
+}​)
+export class SubjectService {​
+  private delurl="https://atdebjoy.com/others/api/perform/del_subject.php";
+  private editurl="https://atdebjoy.com/others/api/perform/update_subject_name.php";
   private url =
-    'https://atdebjoy.com/others/api/perform/get_subjects.php?stud_id=42&pass=1234abcd';
-
+    'https://atdebjoy.com/others/api/perform/get_subjects.php';
   private urlSubjects =
     'https://atdebjoy.com/others/api/perform/get_subjects.php';
     private urlTerm="https://atdebjoy.com/others/api/perform/get_list_term.php";
     private addSubject="https://atdebjoy.com/others/api/perform/add_subject.php";
-  constructor(private http: HttpClient) {}
-  getPosts() {
-    return this.http.get(this.url);
-  }
-  getPosts2() {
-    return this.http.get(
-      'https://jsonblob.com/api/0ef856d0-18dc-11eb-8971-0314eb721e3f'
+  constructor(private http: HttpClient) {​}​
+  getPosts(user_id: string, pwd: string) {​
+    return this.http.get(this.url+ `?stud_id=${​user_id}​&pass=${​pwd}​`);
+  }​
+  delsub(user_id: string, pwd: string, id){​
+    return this.http.post(this.delurl,
+      JSON.stringify({​ sub_id:id, stud_id: user_id, pass: pwd }​)
     );
-  }
-
-  getAllSubjectData(user_id: string, pwd: string) {
+  }​
+  editsub(user_id: string, pwd: string, id1,name){​
+    return this.http.post(this.editurl,JSON.stringify({​sub_id:id1,stud_id:user_id,pass:pwd,sub_name:name}​));
+  }​​
+  getAllSubjectData(user_id: string, pwd: string) {​
     return this.http.get(this.urlSubjects + `?stud_id=${user_id}&pass=${pwd}`);
-  }
-  getSubjectFromId(user_id: string, pwd: string, sub_id: string) {
+  }​
+  getSubjectFromId(user_id: string, pwd: string, sub_id: string) {​
     return this.http.get(
-      this.urlSubjects + `?stud_id=${user_id}&pass=${pwd}&subject=${sub_id}`
+      this.urlSubjects +`?stud_id=${user_id}&pass=${pwd}&subject=${sub_id}`
     );
-  }
-  getSubjectList(user_id: string, pwd: string) {
+  }​
+  getSubjectList(user_id: string, pwd: string) {​
+    console.log(pwd)
     return this.http.get(
-      this.urlSubjects + `?stud_id=${user_id}&pass=${pwd}&list`
+      this.urlSubjects +`?stud_id=${user_id}&pass=${pwd}&list`
     );
-  }
-
-  addsub(user_id: string, pwd: string, body) {
+  }​
+  addsub(user_id: string, pwd: string, body) {​
     return this.http.post(
       this.addSubject,
-      JSON.stringify({ ...body, stud_id: user_id, pass: pwd })
+      JSON.stringify({​ ...body, stud_id: user_id, pass: pwd }​)
     );
-  }
+  }​
+  getTermList(user_id: string, pwd: string){​
+    return this.http.get(this.urlTerm + `?stud_id=${​user_id}​&pass=${​pwd}​`);
+  }​
+}​
+    
+    
+  
+  
 
-  getTermList(user_id: string, pwd: string){
-    return this.http.get(this.urlTerm + `?stud_id=${user_id}&pass=${pwd}`);
-  }
-}
