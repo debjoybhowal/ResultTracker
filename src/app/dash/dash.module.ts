d4df77392b944bbceb7f2d36ecbacbef15c86bc5
Debjoy <debjoybhowal7@gmail.com>
2020-11-10 12:46:54 +0530

    Pritam added contact us functionality

--- src/app/dash/dash.module.ts
+++ src/app/dash/dash.module.ts
@@ -7,7 +7,7 @@ import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { NgCircleProgressModule } from 'ng-circle-progress';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DashComponent, NavComponent, HeaderComponent],
@@ -15,16 +15,19 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     CommonModule,
     DashRoutingModule,
     NgApexchartsModule,
+    FormsModule,
+    ReactiveFormsModule,
     NgCircleProgressModule.forRoot({
-      "backgroundColor": "#FFFFFF",
-      "backgroundPadding": -18,
-      "radius": 60,
-      "toFixed": 1,
-      "titleFontSize":"28",
-      "outerStrokeWidth": 10,
-      "innerStrokeWidth": 1,
-      "showSubtitle":false,
-      "startFromZero": false})
-  ]
+      backgroundColor: '#FFFFFF',
+      backgroundPadding: -18,
+      radius: 60,
+      toFixed: 1,
+      titleFontSize: '28',
+      outerStrokeWidth: 10,
+      innerStrokeWidth: 1,
+      showSubtitle: false,
+      startFromZero: false,
+    }),
+  ],
 })
-export class DashModule { }
+export class DashModule {}
--- src/app/dash/header/header.component.ts
+++ src/app/dash/header/header.component.ts
@@ -1,5 +1,15 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-
+import { ProfileService } from './../profile/profile.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,10 +17,64 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Output() toggleSideBar = new EventEmitter();
-  constructor() {}
+  form: FormGroup;
+  constructor(
+    private profileService: ProfileService,
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private toastr: ToastrService
+  ) {
+    this.form = this.fb.group({
+      message: new FormControl('', [Validators.required]),
+    });
+  }
+  user_id: string;
+  pwd: string;
+  profile;
   showMapModal: boolean = false;
-  ngOnInit(): void {}
+  showContactModal: boolean = false;
+  ngOnInit(): void {
+    if (localStorage.getItem('user_id')) {
+      this.user_id = localStorage.getItem('user_id');
+      this.pwd = localStorage.getItem('pwd');
+      this.profileService
+        .getProfileInfo(this.user_id, this.pwd)
+        .subscribe((response: any) => (this.profile = response.response));
+    }
+  }
+  get f() {
+    return this.form.controls;
+  }
   openMapModal() {
     this.showMapModal = true;
+  }
+  openContactModal() {
+    this.showContactModal = true;
+  }
+  closeContactModal() {
+    this.showContactModal = false;
+    this.form.reset();
+  }
+  onSend() {
+    console.log(this.profile.full_name);
+    console.log(this.profile.email);
+    console.log(this.form.controls.message.value);
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    this.http
+      .post(
+        'https://formspree.io/f/xoqplapv',
+        {
+          Name: this.profile.full_name,
+          From: this.profile.email,
+          Message: this.form.controls.message.value,
+        },
+        { headers: headers }
+      )
+      .subscribe((response) => {
+        console.log(response);
+        this.toastr.success('Message Sent To Developers');
+      });
+    this.showContactModal = false;
+    this.form.reset();
   }
 }
