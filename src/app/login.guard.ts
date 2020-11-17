d6650b65a631ea2f2ff65b8ee3487683d5827621
Debjoy <debjoybhowal7@gmail.com>
2020-11-17 11:23:42 +0530

    Finallized the routing for the intro page

--- src/app/login.guard.ts
+++ src/app/login.guard.ts
@@ -43,7 +43,7 @@ export class LoginGuard implements CanActivate {
           })
         );
     } else {
-      this.router.navigate(['login']);
+      this.router.navigate(['intro']);
       return false;
     }
   }
